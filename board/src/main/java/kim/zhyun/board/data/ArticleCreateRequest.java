b8dd707eef8a3e94d59179b49569bb340cc28061
zhyunk <gimwlgus@gmail.com>
2023-12-13 07:04:01 +0900

    feat : article 등록 controller
    jira issue key : SB-4

--- board/src/main/java/kim/zhyun/board/data/ArticleCreateRequest.java
+++ board/src/main/java/kim/zhyun/board/data/ArticleCreateRequest.java
@@ -1,5 +1,6 @@
 package kim.zhyun.board.data;
 
+import jakarta.validation.constraints.NotEmpty;
 import kim.zhyun.board.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,7 +13,10 @@ import lombok.ToString;
 @Getter
 public class ArticleCreateRequest {
     
+    @NotEmpty(message = "제목을 입력해주세요")
     private String title;
+    
+    @NotEmpty(message = "내용을 입력해주세요")
     private String content;
     
     public static Article to(ArticleCreateRequest request) {
