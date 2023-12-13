73a387292eceaa19a2178278c9cefb31cf61971d
zhyunk <gimwlgus@gmail.com>
2023-12-14 08:40:20 +0900

    chore : profile 분리
    test 코드에서 실행 할 test profile과 일반 사용 할 play profile로 분리하였다.
    프로젝트를 실행하면 play profile이 실행되고 test 코드 실행시 test profile이 실행된다.
    api 호출 테스트시 play profile의 db에 저장됨
    
    jira issue key : SB-9

--- board/src/test/java/kim/zhyun/board/controller/ArticleControllerTest.java
+++ board/src/test/java/kim/zhyun/board/controller/ArticleControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 class ArticleControllerTest {
