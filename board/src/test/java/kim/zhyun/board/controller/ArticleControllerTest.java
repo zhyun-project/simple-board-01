3094408a9b43fbc492bed48052def85766a040e8
zhyunk <gimwlgus@gmail.com>
2023-12-14 17:00:11 +0900

    refactor : application.yml
    prifile을 나누었던 것을 되돌림
    - main 코드의 application.yml에서 profile을 테스트 용과 실행 용으로 나누었던 것을
      test 코드에 application.yml을 작성하는 것으로 수정
    
    jira issue key : SB-9

--- board/src/test/java/kim/zhyun/board/controller/ArticleControllerTest.java
+++ board/src/test/java/kim/zhyun/board/controller/ArticleControllerTest.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -36,7 +35,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 class ArticleControllerTest {
